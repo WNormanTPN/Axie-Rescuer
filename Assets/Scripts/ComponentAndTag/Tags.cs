77a908fc704b4bdfd445d40a176d223335a3b42b
sonyp <vinh123az123az@gmail.com>
2023-10-14 15:45:55 +0700

    Merge Tag

--- Assets/Scripts/ComponentAndTag/Tags.cs
+++ Assets/Scripts/ComponentAndTag/Tags.cs
@@ -4,11 +4,14 @@ namespace AxieRescuer
 {
     public struct PlayerTag : IComponentData { }
     public struct ZombieTag : IComponentData { }
+    public struct ZombieNeedInitTag : IComponentData, IEnableableComponent { }
     public struct BuildingTag : IComponentData { }
     public struct IsDie : IComponentData, IEnableableComponent { }
     public struct DroppedItem : IComponentData, IEnableableComponent { }
     public struct SpawnEnemyTag : IComponentData { }
-    public struct ZombieNeedInitTag : IComponentData, IEnableableComponent { }
     public struct WeaponNeedInitTag : IComponentData, IEnableableComponent { }
     public struct TargetPointTag : IComponentData { }
+    public struct AxieTag : IComponentData { }
+    public struct AxieNeedInitTag : IComponentData, IEnableableComponent { }
+    public struct WildAxieTag : IComponentData, IEnableableComponent { }
 }
